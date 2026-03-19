5f872e6714c2f4aac9ac5600e7046ef605fbe526
Rajarshi Chakraborty <rajarshi29032005@gmail.com>
2026-03-19 18:38:51 +0530

    add llama-3.3 model with my application

---
+++ llm_integration.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+import getpass
+from langchain_groq import ChatGroq
+
+if "GROQ_CLOUD_API_KEY" not in os.environ:
+    os.environ["GROQ_CLOUD_API_KEY"] = getpass.getpass("Enter your Groq API key: ")
+
+llm = ChatGroq(
+    groq_api_key=os.getenv("GROQ_CLOUD_API_KEY"),
+    model="llama-3.3-70b-versatile"
+)
+
+if __name__ == "__main__":
+    response = llm.invoke("How to cook mutton kosha ?")
+    print(response.content)
--- preprocess.py
+++ preprocess.py
@@ -1,11 +1,31 @@
 import json
 
+def extract_metadata(post):    
+    post
+    return {
+        "line-count": 15,
+        "language": "english",
+        "tags": ["AI", "Machine Learning", "Business Use cases of AI"],
+    }
+
+
 def processed_posts(raw_post_path, processed_post_path="data/processed-post.json"):
+    good_post = []
     with open(raw_post_path, encoding="utf-8") as file:
         posts = json.load(file)
-        print(posts)
-        print(type(posts))
+        # print(posts)
+        # print(type(posts))
+        
+        for post in posts :
+            metadata = extract_metadata(post["text"])
+            post_with_metadata = post | metadata
+            
+            good_post.append(post_with_metadata)
+            
+    for x in good_post: 
+        print(x)
 
 
 if __name__ == "__main__":
     processed_posts("data/raw-post.json", "data/processed-post.json")
+
