2b21b58dc2588c927f07c9c7d7421132af509c38
Rajarshi Chakraborty <rajarshi29032005@gmail.com>
2026-03-20 21:51:49 +0530

    created streamlit ui and done the post-generator.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from few_shot_prompting import FewShotPrompting
+from post_generator import generate_post
+
+
+length_options = ["Short", "Medium", "Long"]
+language_options = ["English", "Hinglish", "Bengali"]
 
 
 def main():
@@ -10,11 +15,17 @@ def main():
     with col1:
         selected_tag = st.selectbox("Topic", options=tags)
 
-    # with col2:
-    #     selected_length = st.selectbox("Length", options=length_options)
+    with col2:
+        selected_length = st.selectbox("Length", options=length_options, key="lang_1")
 
-    # with col3:
-    #     selected_language = st.selectbox("Language", options=language_options)
+    with col3:
+        selected_language = st.selectbox(
+            "Language", options=language_options, key="lang_2"
+        )
+
+    if st.button("Generate"):
+        post = generate_post(selected_length, selected_language, selected_tag)
+        st.write(post)
 
 
 if __name__ == "__main__":
