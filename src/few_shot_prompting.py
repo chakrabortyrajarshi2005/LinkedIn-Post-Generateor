926bc5a03ad60d032b343337c85cd73531c7abf8
Rajarshi Chakraborty <rajarshi29032005@gmail.com>
2026-03-20 21:17:38 +0530

    add more functions to few_shot_prompting.py

--- src/few_shot_prompting.py
+++ src/few_shot_prompting.py
@@ -1,17 +1,42 @@
 import json
 import pandas as pd
+
+
 class FewShotPrompting:
     def __init__(self, file_path="data/processed-post.json"):
         self.df = None
         self.unique_tags = None
         self.load_posts(file_path)
-    
-    def load_posts(self,file_path):
-        with open (file_path, encoding="utf-8") as file:
-            posts = json.loads(file)
-            df = pd.json.normalize(posts)
-            
+
+    def load_posts(self, file_path):
+        with open(file_path, encoding="utf-8") as file:
+            posts = json.load(file)
+            self.df = pd.json_normalize(posts)
+            self.df["length"] = self.df["line_count"].apply(self.categorize_length)
+            all_tags = self.df["tags"].apply(lambda x: x).sum()
+            self.unique_tags = list(set(all_tags))
+
+    def categorize_length(self, line_count):
+        if line_count < 5:
+            return "Short"
+        elif 5 <= line_count <= 10:
+            return "Medium"
+        else:
+            return "Long"
+
+    def get_tags(self):
+        return self.unique_tags
+
+    def get_filtered_posts(self, length, language, tag):
+        df_filtered = self.df[
+            (self.df["tags"].apply(lambda tags: tag in tags))
+            & (self.df["language"] == language)
+            & (self.df["length"] == length)
+        ]
+        return df_filtered.to_dict(orient="records")
+
 
 if __name__ == "__main__":
     file_system = FewShotPrompting()
-    pass
+    posts = file_system.get_filtered_posts("Medium", "Hinglish", "Job Search")
+    print(posts)
